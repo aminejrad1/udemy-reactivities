eec3f57c41040ae1b738bcb499486afd6f642dbc
aminejrad1 <mohamedamine.jrad@esprit.tn>
2021-11-21 14:34:14 +0100

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,13 @@ namespace API
             });
 
             services.AddDbContext<DataContext>(opt => { opt.UseSqlite(this._config.GetConnectionString("DefaultConnection")); });
+            services.AddCors( opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
 
         }
 
@@ -52,6 +59,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
